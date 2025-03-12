01c4f15bd43272bbe967937dfa7d452df02873b0
Alex McCaskey <amccaskey@nvidia.com>
2025-03-12 13:55:37 +0000

    wip
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

--- runtime/cudaq/platform/entangler/driver_qpu.h
+++ runtime/cudaq/platform/entangler/driver_qpu.h
@@ -1,6 +1,13 @@
+/****************************************************************-*- C++ -*-****
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
 #pragma once
 #include "cudaq/platform/qpu.h"
-
+#include ""
 namespace cudaq {
 class DriverQPU : public QPU {
 public:
---
+++ runtime/cudaq/platform/entangler/entangler_platform.cpp
@@ -0,0 +1,31 @@
+/****************************************************************-*- C++ -*-****
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#include "cudaq/platform/quantum_platform.h"
+
+namespace cudaq {
+
+
+class EntanglerPlatform : public cudaq::quantum_platform {
+public:
+  EntanglerPlatform() {
+   
+  }
+
+  /// @brief Set the target backend. Here we have an opportunity
+  /// to know the -qpu QPU target we are running on. This function will
+  /// read in the qpu configuration file and search for the PLATFORM_QPU
+  /// variable, and if found, will change from the DefaultQPU to the QPU subtype
+  /// specified by that variable.
+  void setTargetBackend(const std::string &backend) override {
+   
+  }
+};
+} // namespace
+
+CUDAQ_REGISTER_PLATFORM(cudaq::EntanglerPlatform, entangler)
