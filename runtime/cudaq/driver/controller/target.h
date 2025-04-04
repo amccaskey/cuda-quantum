426f5b7b4ac7a23d0b6fcb0a6c7719b8fcfc13c5
Alex McCaskey <amccaskey@nvidia.com>
2025-04-04 20:03:03 +0000

    run clang format
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

--- runtime/cudaq/driver/controller/target.h
+++ runtime/cudaq/driver/controller/target.h
@@ -18,28 +18,27 @@ namespace cudaq::driver {
 class target : public extension_point<target> {
 public:
   virtual void initialize(const config::TargetConfig &) = 0;
-  
-  // loading calibrated waveforms? 
 
-  // allocating static hamiltonian terms 
+  // loading calibrated waveforms?
+
+  // allocating static hamiltonian terms
   virtual void allocate(std::size_t num) = 0;
-  virtual void deallocate(std::size_t num) = 0; 
-  
+  virtual void deallocate(std::size_t num) = 0;
+
   // allocating time dynamic hamiltonian terms
-//   virtual drive_line allocate_drive(...) = 0; 
-//   virtual readout_line allocate_readout(...) = 0; 
+  //   virtual drive_line allocate_drive(...) = 0;
+  //   virtual readout_line allocate_readout(...) = 0;
 
   virtual void apply_opcode(const std::string &opCode,
                             const std::vector<double> &params,
                             const std::vector<std::size_t> &qudits) = 0;
-  
-//   virtual void drive(...) = 0; 
-//   virtual void modulate(...) = 0; 
-  
 
-//   virtual ReadoutResult readout(...) = 0; 
+  //   virtual void drive(...) = 0;
+  //   virtual void modulate(...) = 0;
 
-  virtual std::size_t measure_z(std::size_t qudit) = 0; 
+  //   virtual ReadoutResult readout(...) = 0;
+
+  virtual std::size_t measure_z(std::size_t qudit) = 0;
 };
 
 } // namespace cudaq::driver
